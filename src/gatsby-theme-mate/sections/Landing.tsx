e619f6c612d40de0060938cd108f761f2214235a
Arthur Lapprand <lapprand@gmail.com>
2023-04-14 09:49:50 -0300

    feat: add new landing text

--- src/gatsby-theme-mate/sections/Landing.tsx
+++ src/gatsby-theme-mate/sections/Landing.tsx
@@ -13,6 +13,8 @@ const centerHorizontally = { marginRight: 'auto', marginLeft: 'auto' };
 
 const LandingPage = () => {
   const { name, roles, socialLinks, deterministic } = useSiteQuery();
+  const subheader = "Boosting your continuous integration, delivery infrastructure to increase your release pace."
+  const subheaderParagraph = `The name Haim has its origins in Hebrew and translates to "life." At our company, we hold the belief that software engineering plays a fundamental role in the success of an organization, and it is therefore imperative to treat it as a living organism that can adapt, improve, and evolve.`
 
   return (
     <Section.Container id={SECTION.home} Background={Background}>
@@ -20,7 +22,7 @@ const LandingPage = () => {
         textAlign="center"
         as="h1"
         color="primary"
-        fontSize={[6, 7]}
+        fontSize={[5, 6]}
         mb={[3, 4, 5]}
       >
         {`${name}!`}
@@ -29,12 +31,12 @@ const LandingPage = () => {
       <Heading
         as="h2"
         color="primary"
-        fontSize={[5, 6]}
+        fontSize={[3, 4]}
         mb={[3, 5]}
         textAlign="center"
         style={centerHorizontally}
       >
-        <TextLoop interval={5000}>
+        {/* <TextLoop interval={5000}>
           {roles
             .sort(() => (deterministic ? 1 : Math.random() - 0.5))
             .map((text) => (
@@ -42,12 +44,28 @@ const LandingPage = () => {
                 {text}
               </Text>
             ))}
-        </TextLoop>
+        </TextLoop> */}
+        <Text width={[500, 800]} key={subheader}>
+          {subheader}
+        </Text>
+      </Heading>
+
+      <Heading
+        as="h3"
+        color="primary"
+        fontSize={[2, 3]}
+        mb={[3, 5]}
+        textAlign="center"
+        style={centerHorizontally}
+      >
+        <Text width={[500, 800]} key={subheaderParagraph}>
+          {subheaderParagraph}
+        </Text>
       </Heading>
 
       <Flex alignItems="center" justifyContent="center" flexWrap="wrap">
         {socialLinks.map((sl) => (
-          <Box mx={3} fontSize={[5, 6, 6]} key={sl.name}>
+          <Box mx={3} fontSize={[4, 5, 5]} key={sl.name}>
             <SocialLink {...sl} />
           </Box>
         ))}
